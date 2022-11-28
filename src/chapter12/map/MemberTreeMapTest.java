0ce79e8e7c8fafed3ed17a674d5be54018e42ce2
차준화 <jhcha@gsretail.com>
2022-11-28 17:54:03 +0900

    12장 TreeMap 좀 더 보기

--- src/chapter12/map/MemberTreeMapTest.java
+++ src/chapter12/map/MemberTreeMapTest.java
@@ -2,6 +2,8 @@ package chapter12.map;
 
 import chapter12.collection.Member;
 
+import java.util.*;
+
 public class MemberTreeMapTest {
     public static void main(String[] args) {
         MemberTreeMap treeMap = new MemberTreeMap();
@@ -14,5 +16,24 @@ public class MemberTreeMapTest {
         System.out.println("treeMap = " + treeMap);
         treeMap.removeMember(1);
         System.out.println("treeMap = " + treeMap);
+
+        TreeMap<Integer, String> ist = new TreeMap<>();
+        ist.put(1, "a");
+        ist.put(2, "b");
+        ist.put(3, "c");
+        ist.put(4, "d");
+        ist.put(5, "e");
+        boolean b = ist.containsKey(3);
+        System.out.println("b = " + b);
+        Comparator<? super Integer> comparator = ist.comparator();
+        System.out.println("comparator = " + comparator);
+        Map.Entry<Integer, String> integerStringEntry = ist.ceilingEntry(2);
+        System.out.println("integerStringEntry = " + integerStringEntry);
+        Integer integer = ist.ceilingKey(4);
+        System.out.println("integer = " + integer);
+        NavigableSet<Integer> integers = ist.descendingKeySet();
+        System.out.println("integers = " + integers);
+        Set<Map.Entry<Integer, String>> entries = ist.entrySet();
+        System.out.println("entries = " + entries);
     }
 }
