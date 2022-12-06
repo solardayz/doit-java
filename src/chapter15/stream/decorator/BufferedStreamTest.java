d9aa2c31325f9dd8af4365798dcf0647d7cbcd1f
차준화 <jhcha@gsretail.com>
2022-12-06 10:32:05 +0900

    15장 FileInputStream 테스트

---
+++ src/chapter15/stream/decorator/BufferedStreamTest.java
@@ -0,0 +1,26 @@
+package chapter15.stream.decorator;
+
+import java.io.*;
+
+public class BufferedStreamTest {
+    public static void main(String[] args) {
+
+        long millisecond = 0;
+        try(FileInputStream fis = new FileInputStream("a.zip");
+            FileOutputStream fos = new FileOutputStream("copy.zip");
+            BufferedInputStream bis = new BufferedInputStream(fis);
+            BufferedOutputStream bos = new BufferedOutputStream(fos);
+        ) {
+            millisecond = System.currentTimeMillis();
+
+            int i;
+            while ((i = bis.read()) != -1){
+                bos.write(i);
+            }
+            millisecond = System.currentTimeMillis() - millisecond;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println("millisecond = " + millisecond);
+    }
+}
